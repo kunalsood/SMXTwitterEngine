d45b19b37200a4fdeea2d450b8a716bb21496602
Simon Maddox <simon@simonmaddox.com>
2012-05-09 11:56:22 +0100

    Now requesting access to account store, presenting a UIAlertView if needed for choosing an account

--- SMXTwitterEngine/SMXTwitterEngine.m
+++ SMXTwitterEngine/SMXTwitterEngine.m
@@ -9,11 +9,15 @@
 #import "SMXTwitterEngine.h"
 #import <Twitter/Twitter.h>
 #import <Accounts/Accounts.h>
+#import "UIAlertView+MKBlockAdditions.h"
 
 @interface SMXTwitterEngine ()
 
 + (void) useTwitterFrameworkToSendTweet:(NSString *)tweet completionHandler:(void (^)(id response, NSError *error))handler;
 + (void) useManualOauthToSendTweet:(NSString *)tweet completionHandler:(void (^)(id response, NSError *error))handler;
+
+
++ (void) useAccount:(ACAccount *)account toSendTweet:(NSString *)tweet completionHandler:(void (^)(id response, NSError *error))handler;
 
 @end
 
@@ -31,18 +35,58 @@
 + (void) useTwitterFrameworkToSendTweet:(NSString *)tweet completionHandler:(void (^)(id response, NSError *error))handler
 {
     ACAccountStore *accountStore = [[ACAccountStore alloc] init];
-    NSArray *accounts = [accountStore accountsWithAccountType:[accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter]];
     
-    if (accounts.count == 0){
-        [self useManualOauthToSendTweet:tweet completionHandler:handler];
-    } else {
-        
-    }
+    [accountStore requestAccessToAccountsWithType:[accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter] 
+                            withCompletionHandler:^(BOOL granted, NSError *error){
+                                if (granted){
+                                    NSArray *accounts = [accountStore accountsWithAccountType:[accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter]];
+                                    
+                                    if (accounts.count == 0){
+                                        // No accounts set up. Let's fall back to OAuth
+                                        [self useManualOauthToSendTweet:tweet completionHandler:handler];
+                                    } else {
+                                        
+                                        if (accounts.count == 1){
+                                            // One account set up. Let's use that.
+                                            [SMXTwitterEngine useAccount:[accounts objectAtIndex:0] toSendTweet:tweet completionHandler:handler];
+                                        } else {
+                                            // More than one account set up. Let's ask which one we should use...
+                                            NSArray *titles = [accounts valueForKeyPath:@"username"];
+                                            
+                                            [UIAlertView alertViewWithTitle:NSLocalizedString(@"Choose a Twitter account", @"Choose a Twitter account alert title") 
+                                                                    message:nil
+                                                          cancelButtonTitle:NSLocalizedString(@"Cancel", @"Choose a Twitter account alert cancel button") 
+                                                          otherButtonTitles:titles 
+                                                                  onDismiss:^(int buttonIndex){
+                                                                      [SMXTwitterEngine useAccount:[accounts objectAtIndex:buttonIndex] toSendTweet:tweet completionHandler:handler];
+                                                                  }
+                                                                   onCancel:^(){
+                                                                       handler(nil, [NSError errorWithDomain:@"com.simonmaddox.ios.SMXTwitterEngine" code:101 userInfo:[NSDictionary dictionaryWithObject:NSLocalizedString(@"User Cancelled", @"User Cancelled error message") forKey:NSLocalizedDescriptionKey]]);
+                                                                   }
+                                             ];
+                                            
+                                        }
+                                        
+                                    }
+                                } else {
+                                    if (error == nil){
+                                        error = [NSError errorWithDomain:@"com.simonmaddox.ios.SMXTwitterEngine" code:403 userInfo:[NSDictionary dictionaryWithObject:NSLocalizedString(@"User did not allow access to Twitter accounts", @"User did not allow access to Twitter accounts error message") forKey:NSLocalizedDescriptionKey]];
+                                    }
+                                    
+                                    handler(nil, error);
+                                }
+                            }
+     ];
+}
+
++ (void) useAccount:(ACAccount *)account toSendTweet:(NSString *)tweet completionHandler:(void (^)(id response, NSError *error))handler
+{
+    NSLog(@"Using Account: %@", account);
 }
 
 + (void) useManualOauthToSendTweet:(NSString *)tweet completionHandler:(void (^)(id response, NSError *error))handler
 {
-    
+    NSLog(@"Manually sending tweet");
 }
 
 @end
