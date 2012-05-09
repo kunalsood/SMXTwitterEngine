58f3150597400cd6674a4982a708b15c008f41ee
Simon Maddox <simon@simonmaddox.com>
2012-05-09 11:31:09 +0100

    Initial Commit

---
+++ SMXTwitterEngine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SMXTwitterEngine
+//
+//  Created by Simon Maddox on 09/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
